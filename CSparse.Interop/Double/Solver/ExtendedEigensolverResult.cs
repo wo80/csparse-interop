ace90eb51a03d1a6fec80ead040f02b8615d0da1
wo80 <christian.woltering@tu-dortmund.de>
2018-09-22 15:57:37 +0200

    Add missing files.

---
+++ CSparse.Interop/Double/Solver/ExtendedEigensolverResult.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CSparse.Double.Solver
+{
+    using CSparse.Interop.MKL;
+    using CSparse.Storage;
+
+    public class ExtendedEigensolverResult : ExtendedEigensolverResult<double>
+    {
+        /// <inheritdoc />
+        public ExtendedEigensolverResult(SparseStatus info, int size, int k, double[] e, DenseColumnMajorStorage<double> x, double[] r)
+            : base(info, size, k, e, x, r)
+        {
+        }
+    }
+}
