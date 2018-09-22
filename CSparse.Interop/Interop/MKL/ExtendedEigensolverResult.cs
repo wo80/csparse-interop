7d51db0e697b397faae85b5dd7c3b3876dac0fa4
wo80 <christian.woltering@tu-dortmund.de>
2018-09-22 15:48:57 +0200

    Add MKL extended eigensolver interop.

---
+++ CSparse.Interop/Interop/MKL/ExtendedEigensolverResult.cs
@@ -0,0 +1,74 @@
+﻿
+namespace CSparse.Interop.MKL
+{
+    using CSparse.Storage;
+    using System;
+    using System.Numerics;
+
+    public class ExtendedEigensolverResult<T>
+        where T : struct, IEquatable<T>, IFormattable
+    {
+        protected int size;
+
+        /// <summary>
+        /// Initializes a new instance of the Result class.
+        /// </summary>
+        /// <param name="info">The status returned by Intel's extended eigensolver.</param>
+        /// <param name="size">The matrix size.</param>
+        /// <param name="k">The number of eigenvalues found (k &lt; k0).</param>
+        /// <param name="e">Array of length k0. The first k entries of e are eigenvalues found in the interval.</param>
+        /// <param name="x">Matrix with k0 columns containing the orthonormal eigenvectors corresponding to the
+        /// computed eigenvalues e, with the i-th column of x holding the eigenvector associated with e[i].</param>
+        /// <param name="r">Array of length k0 containing the relative residual vector (in the first k components).</param>
+        public ExtendedEigensolverResult(SparseStatus info, int size, int k, double[] e, DenseColumnMajorStorage<T> x, double[] r)
+        {
+            this.size = size;
+
+            Status = info;
+
+            ConvergedEigenvalues = k;
+
+            EigenValues = CreateEigenValues(e, k);
+            EigenVectors = x;
+
+            Residuals = r;
+        }
+
+        /// <summary>
+        /// Gets the status code returned by FEAST.
+        /// </summary>
+        public SparseStatus Status { get; protected set; }
+        
+        /// <summary>
+        /// Gets the number of converged eigenvalues.
+        /// </summary>
+        public int ConvergedEigenvalues { get; protected set; }
+
+        /// <summary>
+        /// Gets the dense matrix of eigenvectors stored in column major order.
+        /// </summary>
+        public DenseColumnMajorStorage<T> EigenVectors { get; protected set; }
+
+        /// <summary>
+        /// Gets the eigenvalues.
+        /// </summary>
+        public Complex[] EigenValues { get; protected set; }
+
+        /// <summary>
+        /// Gets the residuals vector.
+        /// </summary>
+        public double[] Residuals { get; protected set; }
+
+        protected virtual Complex[] CreateEigenValues(double[] x, int length)
+        {
+            var result = new Complex[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = x[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CSparse.Interop/Interop/MKL/Helper.cs
@@ -0,0 +1,80 @@
+﻿
+namespace CSparse.Interop.MKL
+{
+    using System;
+    using System.Text;
+
+    public static class Helper
+    {
+        public static Version GetVersion()
+        {
+            MKLVersion version = default(MKLVersion);
+
+            // Does this leak memory (char* pointers in MKLVersion struct)?
+
+            NativeMethods.mkl_get_version(ref version);
+
+            return new Version(version.MajorVersion, version.MinorVersion, version.BuildNumber);
+        }
+
+        public static string GetVersionString()
+        {
+            var buffer = new StringBuilder(200);
+
+            NativeMethods.mkl_get_version_string(buffer, 200);
+
+            return buffer.ToString().TrimEnd();
+        }
+
+        public static void SetNumThreads(int nt)
+        {
+            NativeMethods.mkl_set_num_threads(nt);
+        }
+
+        public static int GetMaxThreads()
+        {
+            return NativeMethods.mkl_get_max_threads();
+        }
+
+        /*
+
+        public static int SetCWBR(CBWR cbwr)
+        {
+            return NativeMethods.mkl_cbwr_set((int)cbwr);
+        }
+
+        public static CBWR GetCWBR()
+        {
+            return (CBWR)NativeMethods.mkl_cbwr_get(MKL_CBWR_ALL);
+        }
+
+        public enum CBWR
+        {
+            UNSET_ALL = 0,
+            OFF = 0,
+            BRANCH_OFF = 1,
+            AUTO = 2,
+            COMPATIBLE = 3,
+            SSE2 = 4,
+            SSE3 = 5,
+            SSSE3 = 6,
+            SSE4_1 = 7,
+            SSE4_2 = 8,
+            AVX = 9,
+            AVX2 = 10
+        }
+
+        const int MKL_CBWR_BRANCH = 1;
+        const int MKL_CBWR_ALL = ~0;
+
+        // error codes
+        const int CBWR_SUCCESS = 0;
+        const int CBWR_ERR_INVALID_SETTINGS = -1;
+        const int CBWR_ERR_INVALID_INPUT = -2;
+        const int CBWR_ERR_UNSUPPORTED_BRANCH = -3;
+        const int CBWR_ERR_UNKNOWN_BRANCH = -4;
+        const int CBWR_ERR_MODE_CHANGE_FAILURE = -8;
+
+        //*/
+    }
+}
