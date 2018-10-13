a349cd38dbfa1ff8912544b7c2e03430c64360f6
wo80 <christian.woltering@tu-dortmund.de>
2018-10-13 14:49:16 +0200

    Add METIS interop.

---
+++ CSparse.Interop/Interop/Metis/Helper.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CSparse.Interop.Metis
+{
+    static class Helper
+    {
+        public static int[] CreateArray(int n, int value)
+        {
+            var data = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = value;
+            }
+
+            return data;
+        }
+    }
+}
