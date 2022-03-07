b7c055d518398d7b957fd5a5fcfcd90b9506b0fb
wo80 <christian.woltering@tu-dortmund.de>
2022-03-07 16:53:31 +0100

    Add support for 64bit indices to UMFPACK.

--- CSparse.Interop/Double/Factorization/SuiteSparse/Umfpack_Long.cs
+++ CSparse.Interop/Double/Factorization/SuiteSparse/Umfpack_Long.cs
@@ -4,45 +4,70 @@ namespace CSparse.Double.Factorization.SuiteSparse
     using CSparse.Interop.SuiteSparse.Umfpack;
     using System;
 
+#if X64
+    using SuiteSparse_long = System.Int64;
+#else
+    using SuiteSparse_long = System.Int32;
+#endif
+
     /// <summary>
     /// UMFPACK wrapper.
     /// </summary>
-    public class Umfpack : UmfpackContext<double>
+    public class Umfpack_Long : UmfpackContext<double>
     {
+        SuiteSparse_long[] columnPointers;
+        SuiteSparse_long[] rowIndices;
+
         /// <summary>
         /// Initializes a new instance of the Umfpack class.
         /// </summary>
-        public Umfpack(SparseMatrix matrix)
+        public Umfpack_Long(SparseMatrix matrix)
             : base(matrix)
         {
+            columnPointers = Convert(matrix.ColumnPointers);
+            rowIndices = Convert(matrix.RowIndices);
+        }
+
+        private SuiteSparse_long[] Convert(int[] source)
+        {
+            int size = source.Length;
+
+            var target = new SuiteSparse_long[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                target[i] = source[i];
+            }
+
+            return target;
         }
 
         /// <inheritdoc/>
         protected override void DoInitialize()
         {
-            NativeMethods.umfpack_di_defaults(control.Raw);
+            NativeMethods.umfpack_dl_defaults(control.Raw);
         }
 
         /// <inheritdoc/>
         protected override int DoSymbolic()
         {
-            return NativeMethods.umfpack_di_symbolic(matrix.RowCount, matrix.ColumnCount,
-                matrix.ColumnPointers, matrix.RowIndices, matrix.Values,
+            return (int)NativeMethods.umfpack_dl_symbolic(matrix.RowCount, matrix.ColumnCount,
+                columnPointers, rowIndices, matrix.Values,
                 out symbolic, control.Raw, info.Raw);
         }
 
         /// <inheritdoc/>
         protected override int DoNumeric()
         {
-            return NativeMethods.umfpack_di_numeric(matrix.ColumnPointers, matrix.RowIndices, matrix.Values,
+            return (int)NativeMethods.umfpack_dl_numeric(columnPointers, rowIndices, matrix.Values,
                 symbolic, out numeric, control.Raw, info.Raw);
         }
 
         /// <inheritdoc/>
         protected override int DoFactorize()
         {
-            int status = NativeMethods.umfpack_di_symbolic(matrix.RowCount, matrix.ColumnCount,
-                matrix.ColumnPointers, matrix.RowIndices, matrix.Values,
+            int status = (int)NativeMethods.umfpack_dl_symbolic(matrix.RowCount, matrix.ColumnCount,
+                columnPointers, rowIndices, matrix.Values,
                 out symbolic, control.Raw, info.Raw);
 
             if (status != Constants.UMFPACK_OK)
@@ -50,22 +75,25 @@ namespace CSparse.Double.Factorization.SuiteSparse
                 return status;
             }
 
-            return NativeMethods.umfpack_di_numeric(matrix.ColumnPointers, matrix.RowIndices, matrix.Values,
+            return (int)NativeMethods.umfpack_dl_numeric(columnPointers, rowIndices, matrix.Values,
                 symbolic, out numeric, control.Raw, info.Raw);
         }
 
         /// <inheritdoc/>
         protected override int DoSolve(UmfpackSolve sys, double[] input, double[] result)
         {
-            return NativeMethods.umfpack_di_solve((int)sys, matrix.ColumnPointers, matrix.RowIndices, matrix.Values,
+            return (int)NativeMethods.umfpack_dl_solve((int)sys, columnPointers, rowIndices, matrix.Values,
                 result, input, numeric, control.Raw, info.Raw);
         }
 
         /// <inheritdoc/>
         protected override int DoSolve(UmfpackSolve sys, double[] input, double[] result, int[] wi, double[] wx)
         {
-            return NativeMethods.umfpack_di_wsolve((int)sys, matrix.ColumnPointers, matrix.RowIndices, matrix.Values,
-                result, input, numeric, control.Raw, info.Raw, wi, wx);
+            // TODO: make class member.
+            var wl = new SuiteSparse_long[wi.Length];
+
+            return (int)NativeMethods.umfpack_dl_wsolve((SuiteSparse_long)sys, columnPointers, rowIndices, matrix.Values,
+                result, input, numeric, control.Raw, info.Raw, wl, wx);
         }
 
         /// <inheritdoc/>
@@ -79,12 +107,12 @@ namespace CSparse.Double.Factorization.SuiteSparse
         {
             if (symbolic != IntPtr.Zero)
             {
-                NativeMethods.umfpack_di_free_symbolic(ref symbolic);
+                NativeMethods.umfpack_dl_free_symbolic(ref symbolic);
             }
 
             if (numeric != IntPtr.Zero)
             {
-                NativeMethods.umfpack_di_free_numeric(ref numeric);
+                NativeMethods.umfpack_dl_free_numeric(ref numeric);
             }
         }
     }
